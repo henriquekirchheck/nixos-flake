4de9c44be3287b456b25c99423e83c37648b7a00
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-11-24 16:19:52 -0300

    added fnm config

--- user/app/development/js.nix
+++ user/app/development/js.nix
@@ -1,10 +1,13 @@
 { config, pkgs, lib, ... }:
-
-{
+let
+  fnmConfig = ''eval "$(fnm env --use-on-cd)"'';
+in {
   home.packages = with pkgs; [
     fnm
     bun
     nodejs_20
     nodePackages.pnpm
   ];
+  programs.zsh.initExtra = fnmConfig;
+  programs.bash.initExtra = fnmConfig;
 }
