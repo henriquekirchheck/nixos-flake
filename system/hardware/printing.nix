4b0725022c4d9a36400b64a197eb9a132ed2e986
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-10 10:54:47 -0300

    Added printer driver

--- system/hardware/printing.nix
+++ system/hardware/printing.nix
@@ -3,6 +3,7 @@
 {
   # Enable printing
   services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ epson-escpr ];
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
   services.avahi.openFirewall = true;
