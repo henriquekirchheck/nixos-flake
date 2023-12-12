c5f8e925a41c794a94135eef60206f673b7cdfb9
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-12 18:42:58 -0300

    made it so home-manager uses the right version of hyprland

--- user/wm/hyprland.nix
+++ user/wm/hyprland.nix
@@ -64,6 +64,7 @@ in {
 
   wayland.windowManager.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     xwayland.enable = true;
     plugins = [
       inputs.hyprland-plugins.packages.${pkgs.system}.hyprwinwrap
