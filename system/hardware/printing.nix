7786caa4293dc323a82924f33ca96a24118e97ac
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-12 18:29:52 -0300

    Fixed renamed key in avahi

--- system/hardware/printing.nix
+++ system/hardware/printing.nix
@@ -3,7 +3,9 @@
 {
   # Enable printing
   services.printing.enable = true;
-  services.avahi.enable = true;
-  services.avahi.nssmdns = true;
-  services.avahi.openFirewall = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 }
