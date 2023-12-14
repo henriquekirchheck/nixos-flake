7f3d757c3904b7120e76dab9517a6c217b479369
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-14 15:06:19 -0300

    Added obs

---
+++ system/app/obs.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, lib, ... }:
+
+{
+  environment.systemPackages = [
+    (pkgs.wrapOBS {
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-backgroundremoval
+        obs-pipewire-audio-capture
+        obs-vkcapture
+        obs-vaapi
+        obs-livesplit-one
+        obs-gstreamer
+      ];
+    })
+  ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+}
