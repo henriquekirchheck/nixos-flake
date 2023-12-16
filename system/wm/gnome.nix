3ee3ba2d8556ac9bcabba5b9aed94c673895cdd7
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-16 20:55:28 -0300

    Added gdm

---
+++ system/wm/gnome.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, lib, ... }:
+
+{
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+}
