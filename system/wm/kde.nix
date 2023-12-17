b27c8e1e857b3b7761b8ec4730fcd59babca3597
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-17 12:57:23 -0300

    Change to gdm for now

--- system/wm/kde.nix
+++ system/wm/kde.nix
@@ -11,7 +11,8 @@ let
 in {
   services.xserver.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmawayland";
   programs.dconf.enable = true;
 
   environment.plasma5.excludePackages = with pkgs.libsForQt5; [
