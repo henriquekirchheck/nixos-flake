019858a79ffe04c2abf95d515a1841e148638574
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-20 16:16:40 -0300

    added tex

--- profiles/pc/home.nix
+++ profiles/pc/home.nix
@@ -4,6 +4,7 @@
   imports = [
     ../base/home.nix
     ../../user/app/sandboxing/virtualization.nix
+    ../../user/app/development/tex.nix
     ../../user/app/games/yuzu.nix
     ../../user/app/games/ryujinx.nix
     ../../user/app/games/prismlauncher.nix
---
+++ user/app/development/tex.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+let
+  tex = (pkgs.texlive.combine {
+    inherit (pkgs.texlive) scheme-full
+      dvisvgm dvipng latexmk
+      wrapfig amsmath ulem hyperref capt-of;
+      #(setq org-latex-compiler "lualatex")
+      #(setq org-preview-latex-default-process 'dvisvgm)
+  });
+in
+{
+  home.packages = with pkgs; [
+    tex
+  ];
+}
