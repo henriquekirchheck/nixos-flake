53e29386170981eff3497a1bb1da8c9309377e07
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-24 21:01:10 -0300

    Add workaround for gh problem

--- user/app/development/git.nix
+++ user/app/development/git.nix
@@ -12,7 +12,11 @@
   };
   programs.gh = {
     enable = true;
-    settings.git_protocol = "ssh";
+    settings = {
+      git_protocol = "ssh";
+      # Workaround for https://github.com/nix-community/home-manager/issues/4744
+      version = 1;
+    };
     gitCredentialHelper.enable = true;
   };
 }
