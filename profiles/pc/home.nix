a9c5ef0b8182cdf5973ce8f27220f927844687be
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-27 21:29:53 -0300

    Added blender and godot

--- profiles/pc/home.nix
+++ profiles/pc/home.nix
@@ -5,6 +5,8 @@
     ../base/home.nix
     ../../user/app/sandboxing/virtualization.nix
     ../../user/app/development/tex.nix
+    ../../user/app/development/godot.nix
+    ../../user/app/development/blender.nix
     ../../user/app/games/yuzu.nix
     ../../user/app/games/ryujinx.nix
     ../../user/app/games/prismlauncher.nix
