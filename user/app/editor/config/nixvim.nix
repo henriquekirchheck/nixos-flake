3c6d3dea11c369a35da0e69317b9feff903e63ed
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-03-23 13:06:01 -0300

    Nixvim

--- user/app/editor/config/nixvim.nix
+++ user/app/editor/config/nixvim.nix
@@ -95,20 +95,8 @@
           "<C-e>" = "cmp.mapping.close()";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<S-Tab>" = {
-            action = ''
-              function(fallback)
-                if cmp.visible() then
-                  cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
-                  luasnip.jump(-1)
-                else
-                  fallback()
-                end
-              end
-            '';
-            modes = [ "i" "s" ];
-          };
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
         };
       };
     };
