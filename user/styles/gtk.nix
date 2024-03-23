fa94667638ec905e0d0f0c43798d1555bad37669
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-03-23 13:28:27 -0300

    Temporarily removing gtk styling

--- user/styles/gtk.nix
+++ user/styles/gtk.nix
@@ -1,25 +1,36 @@
-{ config, pkgs, lib, ... }:
-
-{
-	gtk = {
-		enable = true;
-		theme = {
-			package = pkgs.catppuccin-gtk.override {
-				accents = [ "sapphire" ];
-				size = "compact";
-				variant = "mocha";
-				tweaks = [ "black" ];
-			};
-	    name = "Catppuccin-Mocha-Compact-Sapphire-Dark";
-		};
-		iconTheme = {
-			package = pkgs.papirus-icon-theme;
-	    name = "Papirus-Dark";
-		};
-		cursorTheme = {
-			package = pkgs.phinger-cursors;
-			name = "phinger-cursors-light";
-			size = 24;
-		};
-	};
-}
+# { config, pkgs, lib, ... }:
+#let
+#  themePackage = pkgs.catppuccin-gtk.override {
+#    accents = [ "sapphire" ];
+#    size = "compact";
+#    variant = "mocha";
+#    tweaks = [ "black" ];
+#  };
+#  themeName = "Catppuccin-Mocha-Compact-Sapphire-Dark";
+#in {
+#  gtk = {
+#    enable = true;
+#    theme = {
+#      package = themePackage;
+#      name = themeName;
+#    };
+#    iconTheme = {
+#      package = pkgs.papirus-icon-theme;
+#      name = "Papirus-Dark";
+#    };
+#    cursorTheme = {
+#      package = pkgs.phinger-cursors;
+#      name = "phinger-cursors-light";
+#      size = 24;
+#    };
+#  };
+#  xdg.configFile = {
+#    "gtk-4.0/assets".source =
+#      "${themePackage}/share/themes/${themeName}/gtk-4.0/assets";
+#    "gtk-4.0/gtk.css".source =
+#      "${themePackage}/share/themes/${themeName}/gtk-4.0/gtk.css";
+#    "gtk-4.0/gtk-dark.css".source =
+#      "${themePackage}/share/themes/${themeName}/gtk-4.0/gtk-dark.css";
+#  };
+#}
+{ }
