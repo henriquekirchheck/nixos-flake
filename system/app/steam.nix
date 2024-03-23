d5b478338d8b82f1f90e69a483ba8093331d1cd8
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-03-23 10:58:24 -0300

    Add some game extras to steam

--- system/app/steam.nix
+++ system/app/steam.nix
@@ -3,5 +3,6 @@
 {
   hardware.opengl.driSupport32Bit = true;
   programs.steam.enable = true;
-  environment.systemPackages = with pkgs; [ steam ];
+  environment.systemPackages = with pkgs; [ steam gamescope mangohud ];
+  programs.gamemode.enable = true;
 }
