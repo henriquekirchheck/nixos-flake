99d32b0170839b90b8c3f6746c0aa0242f1d4d1d
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-03-29 16:18:48 -0300

    Changed gtk

--- user/styles/gtk.nix
+++ user/styles/gtk.nix
@@ -1,20 +1,19 @@
 { config, pkgs, lib, ... }:
-#let
-#  themePackage = pkgs.catppuccin-gtk.override {
-#    accents = [ "sapphire" ];
-#    size = "compact";
-#    variant = "mocha";
-#    tweaks = [ "black" ];
-#  };
-#  themeName = "Catppuccin-Mocha-Compact-Sapphire-Dark";
-#in 
-{
+let
+  themePackage = pkgs.catppuccin-gtk.override {
+    accents = [ "sapphire" ];
+    size = "compact";
+    variant = "mocha";
+    tweaks = [ "black" ];
+  };
+  themeName = "Catppuccin-Mocha-Compact-Sapphire-Dark";
+in {
   gtk = {
-    #    enable = true;
-    #    theme = {
-    #      package = themePackage;
-    #      name = themeName;
-    #    };
+    enable = true;
+    theme = {
+      package = themePackage;
+      name = themeName;
+    };
     iconTheme = {
       package = pkgs.papirus-icon-theme;
       name = "Papirus-Dark";
@@ -25,12 +24,12 @@
       size = 24;
     };
   };
-  #  xdg.configFile = {
-  #    "gtk-4.0/assets".source =
-  #      "${themePackage}/share/themes/${themeName}/gtk-4.0/assets";
-  #    "gtk-4.0/gtk.css".source =
-  #      "${themePackage}/share/themes/${themeName}/gtk-4.0/gtk.css";
-  #    "gtk-4.0/gtk-dark.css".source =
-  #      "${themePackage}/share/themes/${themeName}/gtk-4.0/gtk-dark.css";
-  #  };
+  xdg.configFile = {
+    "gtk-4.0/assets".source =
+      "${themePackage}/share/themes/${themeName}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source =
+      "${themePackage}/share/themes/${themeName}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source =
+      "${themePackage}/share/themes/${themeName}/gtk-4.0/gtk-dark.css";
+  };
 }
