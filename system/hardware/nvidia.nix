0a3f37b95e93dabef86b8023fe10e84d68f8142d
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-05-23 17:58:54 -0300

    Add nvidia specializations

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -39,14 +39,28 @@
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     # package = config.boot.kernelPackages.nvidiaPackages.beta;
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.42.02";
-      sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-      sha256_aarch64 = "";
-      openSha256 = "";
-      settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-      persistencedSha256 =
-        "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
+  };
+
+  specialisation = {
+    "nvidia-stable".configuration = {
+      environment.etc."specialisation".text = "nvidia-stable";
+      hardware.nvidia.package =
+        config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+    "nvidia-beta".configuration = {
+      environment.etc."specialisation".text = "nvidia-beta";
+      hardware.nvidia.package =
+        config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "555.42.02";
+          sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+          sha256_aarch64 = "";
+          openSha256 = "";
+          settingsSha256 =
+            "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+          persistencedSha256 =
+            "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
+        };
     };
   };
 }
