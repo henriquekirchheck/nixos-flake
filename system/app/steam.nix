7abdc93604010824424252fd2f535142a10e5974
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-07-03 20:19:06 -0300

    Steam

--- system/app/steam.nix
+++ system/app/steam.nix
@@ -2,7 +2,17 @@
 
 {
   hardware.opengl.driSupport32Bit = true;
-  programs.steam.enable = true;
-  environment.systemPackages = with pkgs; [ steam gamescope mangohud ];
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
+    localNetworkGameTransfers.openFirewall = true;
+    protontricks.enable = true;
+    remotePlay.openFirewall = true;
+  };
+  environment.systemPackages = with pkgs; [ mangohud ];
   programs.gamemode.enable = true;
 }
