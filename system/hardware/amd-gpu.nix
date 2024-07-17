dce65fbad8d22270fd4b4dd31a30304d8a1f7a61
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-07-16 23:29:26 -0300

    Amd support

--- system/hardware/amd-gpu.nix
+++ system/hardware/amd-gpu.nix
@@ -4,17 +4,12 @@
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
-    driSupport = true;
     driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      rocm-opencl-icd
-      rocm-opencl-runtime
-    ];
+    extraPackages = with pkgs; [ rocm-opencl-icd rocm-opencl-runtime ];
   };
 
-  systemd.tmpfiles.rules = [
-    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
-  ];
+  systemd.tmpfiles.rules =
+    [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
 
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
