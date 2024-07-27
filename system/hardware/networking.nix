91d22445ec5b9651048647748a18f89600c55c8f
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-07-27 01:08:23 -0300

    Added tailscale magicdns

--- system/hardware/networking.nix
+++ system/hardware/networking.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  networking.networkmanager.enable = true;
+  networking = {
+    networkmanager.enable = true;
+    nameservers = [ "100.100.100.100" "1.1.1.1" "1.0.0.1" "8.8.8.8" "9.9.9.9" ];
+    search = [ "example.ts.net" ];
+  };
   services.zerotierone.enable = true;
   services.tailscale = {
     enable = true;
