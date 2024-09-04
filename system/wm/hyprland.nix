e12f00425201d12cad7438a7655a7792b8bc985c
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-09-04 10:16:11 -0300

    remove xdph

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -19,7 +19,6 @@
     hyprland = {
       enable = true;
       xwayland.enable = true;
-      portalPackage = pkgs.xdg-desktop-portal-hyprland;
       package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     };
   };
