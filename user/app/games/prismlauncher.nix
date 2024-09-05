930b3d74f453b4d6d050752274fa02888e42cdbb
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-09-04 22:59:09 -0300

    remove custom glfw wayland

--- user/app/games/prismlauncher.nix
+++ user/app/games/prismlauncher.nix
@@ -1,7 +1,6 @@
 { config, pkgs, lib, ... }:
 let
   launcher = (pkgs.prismlauncher.override {
-    glfw = pkgs.glfw-wayland-minecraft;
     gamemodeSupport = true;
   });
 in { home.packages = [ launcher ]; }
