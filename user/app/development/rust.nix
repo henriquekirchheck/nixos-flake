4c7fa09e2a561688e1e879aa4a06fa4281eacde7
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-10-14 16:24:50 -0300

    Fix rustsrc

--- user/app/development/rust.nix
+++ user/app/development/rust.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ cargo rustc pkg-config ];
+  home.packages = with pkgs; [ rustc cargo gcc rustfmt clippy ];
+  home.sessionVariables = {
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  };
 }
