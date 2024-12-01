ab3f6b095c1cc5d61dc1179fa914182dfd4ec3d5
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-12-01 01:26:56 -0300

    fixes

--- system/extras/fonts.nix
+++ system/extras/fonts.nix
@@ -3,34 +3,27 @@
 {
   # Fonts are nice to have
   fonts.fontDir.enable = true;
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-    roboto
-    roboto-serif
-    liberation_ttf
-    freefont_ttf
-    dejavu_fonts
-    unifont
-    (nerdfonts.override {
-      fonts = [
-        "FiraCode"
-        "Inconsolata"
-        "Iosevka"
-        "JetBrainsMono"
-        "Meslo"
-        "RobotoMono"
-        "Ubuntu"
-        "UbuntuMono"
-      ];
-    })
-    fira-code
-    fira-code-symbols
-    inconsolata
-    iosevka
-    jetbrains-mono
-    meslo-lg
-    ubuntu_font_family
-  ];
+  fonts.packages = with pkgs;
+    [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      roboto
+      roboto-serif
+      liberation_ttf
+      freefont_ttf
+      dejavu_fonts
+      unifont
+      fira-code
+      fira-code-symbols
+      inconsolata
+      iosevka
+      jetbrains-mono
+      meslo-lg
+      ubuntu_font_family
+    ] ++ (builtins.attrValues {
+      inherit (nerd-fonts)
+        fira-code inconsolata iosevka jetbrains-mono meslo-lg roboto-mono ubuntu
+        ubuntu-mono;
+    });
 }
