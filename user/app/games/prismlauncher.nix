e4f71cf9e3fdc0bb2698ebee736cbc4dace614f9
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-12-16 23:15:05 -0300

    minceraft

--- user/app/games/prismlauncher.nix
+++ user/app/games/prismlauncher.nix
@@ -1,16 +1,8 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ config, pkgs, lib, ... }:
 let
-  launcher = (
-    pkgs.prismlauncher.override {
-      gamemodeSupport = true;
-    }
-  );
-in
-{
-  home.packages = [ launcher ];
-}
+  launcher = (pkgs.prismlauncher.override {
+    gamemodeSupport = true;
+    jdks = [ pkgs.jdk21 pkgs.jdk17 pkgs.jdk8 pkgs.graalvm-ce ];
+    additionalPrograms = [ pkgs.kdePackages.kdialog ];
+  });
+in { home.packages = [ launcher ]; }
