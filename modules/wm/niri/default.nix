38055105fa7288fd83a04af4e3c9951e680e5d51
Henrique Kirch Heck <me@henriquekh.dev.br>
2025-11-16 12:15:31 -0300

    change portal

--- modules/wm/niri/default.nix
+++ modules/wm/niri/default.nix
@@ -21,6 +21,7 @@
       "org.freedesktop.impl.portal.FileChooser" = "gtk";
       "org.freedesktop.impl.portal.Access" = "gtk";
       "org.freedesktop.impl.portal.Notification" = "gtk";
+      "org.freedesktop.impl.portal.OpenURI" = "gtk";
       "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
     };
   };
