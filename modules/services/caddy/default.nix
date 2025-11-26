55e04d0ea7795454081881201b1bdfa3cfcc6a9e
Henrique Kirch Heck <me@henriquekh.dev.br>
2025-11-26 00:13:02 -0300

    update flake

--- modules/services/caddy/default.nix
+++ modules/services/caddy/default.nix
@@ -7,7 +7,7 @@
     package = (
       pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-        hash = "sha256-RLOwzx7+SH9sWVlr+gTOp5VKlS1YhoTXHV4k6r5BJ3U=";
+        hash = "sha256-ea8PC/+SlPRdEVVF/I3c1CBprlVp1nrumKM5cMwJJ3U=";
       }
     );
   };
