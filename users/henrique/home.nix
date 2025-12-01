d11e925aa5748981f00e988e600cb9230da9a90b
Henrique Kirch Heck <me@henriquekh.dev.br>
2025-12-01 08:27:00 -0300

    remove caelestia

--- users/henrique/home.nix
+++ users/henrique/home.nix
@@ -46,7 +46,6 @@
     ../../modules/wm/niri/home.nix
     ../../modules/programs/notification/mako.nix
     ../../modules/programs/rofi
-    ../../modules/programs/quickshell/caelestia
     ../../modules/programs/wallpaper/swww.nix
 
     ../../modules/styles/fonts/home.nix
