248eef03a17b95a0cbdd4584d6b2e49bbfffe77b
Henrique Kirch Heck <me@henriquekh.dev.br>
2025-12-31 21:56:30 -0300

    Add more graalvm to minecraft to test performance

--- modules/programs/game/prismlauncher/default.nix
+++ modules/programs/game/prismlauncher/default.nix
@@ -4,11 +4,14 @@
   home.packages = [
     (pkgs.prismlauncher.override {
       gamemodeSupport = true;
-      jdks = [
-        pkgs.jdk21
-        pkgs.jdk17
-        pkgs.jdk8
+      jdks = with pkgs; [
+	javaPackages.compiler.temurin-bin.jre-25
+	javaPackages.compiler.temurin-bin.jre-21
+	javaPackages.compiler.temurin-bin.jre-17
+	javaPackages.compiler.temurin-bin.jre-8
         pkgs.graalvmPackages.graalvm-ce
+        pkgs.graalvmPackages.graalvm-oracle
+        pkgs.graalvmPackages.graalvm-oracle_17
       ];
       additionalPrograms = [ pkgs.kdePackages.kdialog ];
     })
