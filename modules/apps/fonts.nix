aa8f8e25eb0c8bf9350abf2d6ba3f0d94e8f6c0e
Henrique Kirch Heck <me@henriquekh.dev.br>
2026-07-09 01:37:38 -0300

    add microsoft fonts

--- modules/apps/fonts.nix
+++ modules/apps/fonts.nix
@@ -1,36 +1,48 @@
-{
-  den.aspects.apps.provides.fonts.nixos =
-    { pkgs, ... }:
-    {
-      fonts = {
-        fontDir.enable = true;
-        enableDefaultPackages = true;
-        packages =
-          (with pkgs; [
-            noto-fonts
-            noto-fonts-cjk-sans
-            noto-fonts-color-emoji
-            roboto
-            roboto-serif
-            liberation_ttf
-            freefont_ttf
-            dejavu_fonts
-            unifont
-            material-symbols
-          ])
-          ++ (builtins.attrValues {
-            inherit (pkgs.nerd-fonts)
-              fira-code
-              inconsolata
-              iosevka
-              jetbrains-mono
-              meslo-lg
-              roboto-mono
-              ubuntu
-              ubuntu-mono
-              symbols-only
-              ;
-          });
+{ den, ... }: {
+  den.aspects.apps.provides.fonts = {
+    includes = [
+      (den._.unfree [
+        "corefonts"
+        "vista-fonts"
+      ])
+    ];
+
+    nixos =
+      { pkgs, ... }:
+      {
+        fonts = {
+          fontDir.enable = true;
+          enableDefaultPackages = true;
+          packages =
+            (with pkgs; [
+              noto-fonts
+              noto-fonts-cjk-sans
+              noto-fonts-color-emoji
+              roboto
+              roboto-serif
+              liberation_ttf
+              freefont_ttf
+              dejavu_fonts
+              unifont
+              material-symbols
+              # Fucking windows fonts
+              corefonts
+              vista-fonts
+            ])
+            ++ (builtins.attrValues {
+              inherit (pkgs.nerd-fonts)
+                fira-code
+                inconsolata
+                iosevka
+                jetbrains-mono
+                meslo-lg
+                roboto-mono
+                ubuntu
+                ubuntu-mono
+                symbols-only
+                ;
+            });
+        };
       };
-    };
+  };
 }
